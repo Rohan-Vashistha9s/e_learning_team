7e9b2bf285137d5f03fdb61631add2702b3b08b6
anubhootipandey <anubhootipandey05@gmail.com>
2024-08-31 10:25:50 +0530

    courses and blog section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.css'
-import Home from './components/HomeSection/Home'
-import Membership from './components/Membership/Membership'
-import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-
-
-
+import './App.css';
+import Blog from './components/Blog/Blog';
+import Courses from './components/Courses/Courses';
+import Footer from './components/Footer/Footer';
+import Home from './components/HomeSection/Home';
+import Membership from './components/Membership/Membership';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-
-    <BrowserRouter>
+    <Router>
     <Routes>
     <Route exact path='/' element={<Home />} />
+    <Route exact path='/courses' element={<Courses />} />
     <Route exact path='/members' element={<Membership />} />
+    <Route exact path='/blog' element={<Blog />} />
     </Routes>
-    </BrowserRouter>
+    <Footer />
+    </Router>
     </>
   )
 }
